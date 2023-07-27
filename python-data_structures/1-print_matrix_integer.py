3727530f2aebdbe88bbe2ad27d4f8842dfe31469
TshegoR24 <cavinrakoma@gmail.com>
2023-07-27 03:19:42 +0200

    Data Structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,18 +1,13 @@
 def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    cols = len(matrix[0]) if rows > 0 else 0
-
-    for i in range(rows):
-        for j in range(cols):
-            # Using str.format() to print integers without converting them to strings
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()
-
-# Example usage:
-matrix_example = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix_example)
+  """Prints a matrix of integers.
+  Args:
+    matrix: A list of lists of integers.
+  """
+  # Iterate over the rows of the matrix.
+  for row in matrix:
+    # Iterate over the columns of the row.
+    for column in row:
+      # Print the column.
+      print(str(column), end=" ")
+    # Print a newline.
+    print()
