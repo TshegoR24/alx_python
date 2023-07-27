fafd1a77d6dca00357a4826ab95645b031b18a49
TshegoR24 <cavinrakoma@gmail.com>
2023-07-27 14:48:13 +0200

    Datastructure

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,20 @@
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:>4}".format(matrix[i][j]), end="")
+    rows = len(matrix)
+    cols = len(matrix[0]) if rows > 0 else 0
+
+    for i in range(rows):
+        for j in range(cols):
+            # Using str.format() to print integers without converting them to strings
+            print("{:d}".format(matrix[i][j]), end=" ")
         print()
+
+# Example usage:
+matrix_example = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix_example)
+
 
