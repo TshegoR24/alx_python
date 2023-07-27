bf878847bb6e23f469b86219e75fbea61c9f532e
TshegoR24 <cavinrakoma@gmail.com>
2023-07-27 15:40:03 +0200

    more data structure

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,21 +1,9 @@
 def update_dictionary(a_dictionary, key, value):
-  """
-  Replaces or adds key/value in a dictionary.
-  Args:
-    a_dictionary: A dictionary.
-    key: A string.
-    value: Any type.
-  Returns:
-    A new dictionary with the updated key/value pair.
-  """
-  # Check if the input dictionary is valid.
-  if not isinstance(a_dictionary, dict):
-    raise ValueError("a_dictionary must be a dictionary.")
-  # Check if the input key is a string.
-  if not isinstance(key, str):
-    raise ValueError("key must be a string.")
-  # Create a new dictionary with the updated key/value pair.
-  new_dictionary = a_dictionary.copy()
-  new_dictionary[key] = value
-  # Return the new dictionary.
-  return new_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
+
+# Example usage:
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+update_dictionary(my_dict, 'b', 10)  # Replaces the value of 'b' with 10
+update_dictionary(my_dict, 'd', 4)   # Adds a new key-value pair 'd': 4
+print(my_dict)
