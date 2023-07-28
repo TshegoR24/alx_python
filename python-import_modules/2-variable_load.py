fc92cdf63a45e292e52f40743096adfa91c0b85c
TshegoR24 <cavinrakoma@gmail.com>
2023-07-28 16:13:53 +0200

    import modules

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,10 +1,5 @@
-import sys
-def main():
-    with open("variable_load_2.py", "r") as f:
-        lines = f.readlines()
-    for line in lines:
-        if line.startswith("a = "):
-            a = line.split("=")[1].strip()
-            print(a)
-if __name__ == "__main__":
-    main()
+# This is a comment. It will not be executed when the file is imported.
+# Import the variable `a` from the file `variable_load_2.py`.
+from variable_load_2 import a
+# Print the value of the variable `a`.
+print(a)
