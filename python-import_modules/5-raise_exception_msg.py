a51c7fd5fa12539ba328f7864dcee5e2a4f0d18f
TshegoR24 <cavinrakoma@gmail.com>
2023-07-28 10:44:39 +0200

    import modules

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,11 +1,2 @@
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as ne:
-        print(f"NameError: {ne}")
-
-# Test the function
-try:
-    raise_exception_msg("Custom message for the exception")
-except NameError as ne:
-    print(f"Caught the exception: {ne}")
+    raise NameError(message)
