0d8b7b1b12835c0a5f35369d5da211cf4dd57543
TshegoR24 <cavinrakoma@gmail.com>
2023-07-28 15:59:56 +0200

    data structures

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,22 +1,25 @@
 def update_dictionary(a_dictionary, key, value):
   """
   Replaces or adds key/value in a dictionary.
+  key argument will be always a string
+  value argument will be any type
+  If a key exists in the dictionary, the value will be replaced
+  If a key doesn't exist in the dictionary, it will be created
   Args:
-    a_dictionary: A dictionary.
-    key: A string.
-    value: Any type.
+    a_dictionary: The dictionary to be updated.
+    key: The key to be updated or added.
+    value: The value to be associated with the key.
   Returns:
-    A new dictionary with the updated key/value pair.
+    The updated dictionary.
   """
-  # Check if the input dictionary is valid.
-  if not isinstance(a_dictionary, dict):
-    raise ValueError("a_dictionary must be a dictionary.")
-  # Check if the input key is a string.
-  if not isinstance(key, str):
-    raise ValueError("key must be a string.")
-  # Create a new dictionary with the updated key/value pair.
-  new_dictionary = a_dictionary.copy()
-  new_dictionary[key] = value
-  # Return the new dictionary.
-  return new_dictionary
+  # Check if the key exists in the dictionary.
+  if key in a_dictionary:
+    # If the key exists, replace the value.
+    a_dictionary[key] = value
+  else:
+    # If the key doesn't exist, add it with the given value.
+    a_dictionary[key] = value
+  # Return the updated dictionary.
+  return a_dictionary
+
 
