c80916e1b9decdd300824c28e25d589f3e259b5f
TshegoR24 <cavinrakoma@gmail.com>
2023-07-28 10:56:50 +0200

    import modules

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,8 +1,15 @@
 import sys
-def main():
-    print("Number of arguments: {}.".format(len(sys.argv)))
-    if len(sys.argv) > 0:
-        for i in range(1, len(sys.argv)):
-            print("Argument {}: {}".format(i, sys.argv[i]))
+
+def print_arguments(argv):
+    num_arguments = len(argv) - 1  # The first argument is the script name itself
+
+    print(f"Number of argument(s): {num_arguments}", end='')
+    if num_arguments == 0:
+        print(" .")
+    else:
+        print(":")
+        for i, arg in enumerate(argv[1:], start=1):
+            print(f"{i}: {arg}")
+
 if __name__ == "__main__":
-    main()
+    print_arguments(sys.argv)
