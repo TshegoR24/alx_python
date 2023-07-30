1e168ceb7006dd5abf4283d1a05774d04167812b
TshegoR24 <cavinrakoma@gmail.com>
2023-07-30 20:22:57 +0200

    python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,19 @@
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self._size = size
     @property
     def size(self):
         return self._size
     @size.setter
     def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self._size = size
     def area(self):
         return self._size * self._size
