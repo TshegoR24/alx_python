6abdfa1b61e776955252cbb6ed0cfde79587c682
TshegoR24 <cavinrakoma@gmail.com>
2023-07-30 10:43:07 +0200

    python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,24 +1,13 @@
 class Square:
     def __init__(self, size):
-        self.__size = size
-
+        self._size = size
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, size):
+        self._size = size
     def area(self):
-        return self.__size * self.__size
-
+        return self._size * self._size
     def perimeter(self):
-        return 4 * self.__size
-
-    def __str__(self):
-        return f"Square with side length {self.__size}"
-
-# Example usage:
-if __name__ == "__main__":
-    square1 = Square(5)
-    print(square1)  # Output: Square with side length 5
-    print("Area:", square1.area())  # Output: Area: 25
-    print("Perimeter:", square1.perimeter())  # Output: Perimeter: 20
-
-    square2 = Square(10)
-    print(square2)  # Output: Square with side length 10
-    print("Area:", square2.area())  # Output: Area: 100
-    print("Perimeter:", square2.perimeter())  # Output: Perimeter: 40
+        return 4 * self._size
