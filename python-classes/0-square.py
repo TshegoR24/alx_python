17125241922e3476b3d37e96c7ba8e5b6bca8b34
TshegoR24 <cavinrakoma@gmail.com>
2023-07-31 09:17:47 +0200

    python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,21 +1,26 @@
 class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self._size = size
-    @property
-    def size(self):
-        return self._size
-    @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self._size = size
-    def area(self):
-        return self._size * self._size
-    def perimeter(self):
-        return 4 * self._size
+    def __init__(self, size):
+        self.__size = size
+
+    def __str__(self):
+        return f"<class '{type(self).__module__}.{type(self).__name__}'>"
+
+    def __repr__(self):
+        return f"{{'_Square__size': {self.__size}}}"
+
+    def __getattr__(self, item):
+        if item == 'size':
+            raise AttributeError("'Square' object has no attribute 'size'")
+        elif item == '__size':
+            raise AttributeError("'Square' object has no attribute '__size'")
+        else:
+            raise AttributeError(f"'Square' object has no attribute '{item}'")
+
+
+# Test
+square_instance = Square(3)
+print(square_instance)
+print(square_instance.__dict__)
+print(square_instance.size)
+print(square_instance.__size)
+
