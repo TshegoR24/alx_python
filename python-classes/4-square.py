4efdde5eb4c0485bab5cc42e5b08c07d77afac6c
TshegoR24 <cavinrakoma@gmail.com>
2023-07-31 10:03:36 +0200

    python classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,10 +1,15 @@
-def print_pattern():
-    pattern1 = "###\n" * 3
-    separator = "--\n"
-    pattern2 = "##########\n" * 10
-    result = pattern1 + separator + pattern2 + separator
-    print(result)
-
-
-print_pattern()
+print("###")
+print("###")
+print("--")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("--")
+print("--")
 
