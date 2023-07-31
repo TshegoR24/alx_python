dfd0eef99007db6fe707d8df468827e7f345bb02
TshegoR24 <cavinrakoma@gmail.com>
2023-07-31 09:58:36 +0200

    python classes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,18 +1,20 @@
 class Square:
+    # Private instance attribute: size
     def __init__(self, size=0):
-        self.__size = size
-
+        self._size = size
+    # Property def size(self): to retrieve it
     @property
     def size(self):
-        return self.__size
-
+        return self._size
+    # Property setter def size(self, value): to set it:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
+        self._size = value
+    # Instantiation with optional size: def __init__(self, size=0):
+    # Public instance method: def area(self): that returns the current square area
     def area(self):
-        return self.__size ** 2
+        return self._size * self._size
