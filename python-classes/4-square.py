76167d2508850a8db1e3e3b2cb6cd9ec38fe48a7
TshegoR24 <cavinrakoma@gmail.com>
2023-07-31 09:37:39 +0200

    python classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,25 +1,10 @@
-class Square:
-    def __init__(self, size=0):
-        self.__size = size
+def print_pattern():
+    pattern1 = "###\n" * 3
+    separator = "--\n"
+    pattern2 = "##########\n" * 10
+    result = pattern1 + separator + pattern2 + separator
+    print(result)
 
-    @property
-    def size(self):
-        return self.__size
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+print_pattern()
 
-    def area(self):
-        return self.__size * self.__size
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
