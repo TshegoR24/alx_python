18e135595723f4280af4aff6715cc520cd2822d6
TshegoR24 <cavinrakoma@gmail.com>
2023-08-02 10:06:41 +0200

    python classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,28 @@
 class Square:
+    """
+    This class defines a square by its size.
+    Attributes:
+        size (int): The size of the square.
+    """
     def __init__(self, size):
+        """
+        This method initializes the size of the square.
+        Args:
+            size (int): The size of the square.
+        """
         self.size = size
-    @property
-    def size(self):
-        return self.__size
-    @size.setter
-    def size(self, size):
-        self.__size = size
+    def area(self):
+        """
+        This method calculates the area of the square.
+        Returns:
+            int: The area of the square.
+        """
+        return self.size ** 2
+    def perimeter(self):
+        """
+        This method calculates the perimeter of the square.
+        Returns:
+            int: The perimeter of the square.
+        """
+        return 4 * self.size
 
