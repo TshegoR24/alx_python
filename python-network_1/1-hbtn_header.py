b3ad2531b02a4681e326a9313f48d62acf6f3e60
TshegoR24 <cavinrakoma@gmail.com>
2023-08-09 11:22:14 +0200

    python networks

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,23 +1,8 @@
 import requests
 import sys
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <url>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-
-    try:
-        response = requests.get(url)
-        if 'X-Request-Id' in response.headers:
-            x_request_id = response.headers['X-Request-Id']
-            print(f"X-Request-Id: {x_request_id}")
-        else:
-            print("X-Request-Id not found in response headers.")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+url = sys.argv[1]
+response = requests.get(url)
+if response.status_code == 200:
+    print("X-Request-Id:", response.headers.get('X-Request-Id'))
+else:
+    print("Error:", response.status_code)
