8ce3b07ec9eb7b397e78c8169b6aefa5af6ade8d
TshegoR24 <cavinrakoma@gmail.com>
2023-08-09 11:24:25 +0200

    python networks

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,22 +1,7 @@
-import sys
 import requests
-
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <URL> <email>")
-        return
-
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = {'email': email}
-    response = requests.post(url, data=data)
-
-    if response.status_code == 200:
-        print("Response body:")
-        print(response.text)
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-
-if __name__ == "__main__":
-    main()
+import sys
+url = sys.argv[1]
+email = sys.argv[2]
+data = {'email': email}
+response = requests.post(url, data=data)
+print(response.text)
