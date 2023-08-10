b0f5791e18fde0cdb9a7d060da417332057b39de
TshegoR24 <cavinrakoma@gmail.com>
2023-08-10 15:55:19 +0200

    python networks

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,60 +1,22 @@
-"""
-This module defines a class that sends a GET request to a given URL and displays the value of the X-Request-Id header from the response.
-"""
-
 import requests
 import sys
 
-class XRequestIdFetcher:
-    """
-    A class for fetching and displaying the X-Request-Id header from a given URL's response.
-
-    Attributes:
-        url (str): The URL to send the GET request to.
-    """
-
-    def __init__(self, url):
-        """
-        Initialize the XRequestIdFetcher class with a URL.
-
-        Args:
-            url (str): The URL to send the GET request to.
-        """
-        self.url = url
-
-    def get_x_request_id(self):
-        """
-        Send a GET request to the provided URL and display the value of the X-Request-Id header.
-
-        Returns:
-            None
-        """
-        try:
-            response = requests.get(self.url)
-            response.raise_for_status()  # Raise an exception if the response status code is not 2xx
-            x_request_id = response.headers.get('X-Request-Id')
-            if x_request_id:
-                print(f"X-Request-Id: {x_request_id}")
-            else:
-                print("X-Request-Id header not found in the response.")
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            sys.exit(1)
-
-def fetch_x_request_id(url):
-    """
-    Fetch the X-Request-Id header from a provided URL.
-
-    Args:
-        url (str): The URL to send the GET request to.
-
-    Returns:
-        None
-    """
-    fetcher = XRequestIdFetcher(url)
-    fetcher.get_x_request_id()
+def send_post_request(url, email):
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    return response.text
 
 if __name__ == "__main__":
-    input_url = input("Enter the URL: ")
-    fetch_x_request_id(input_url)
+    if len(sys.argv) != 3:
+        print("Usage: hr@holbertonschool.com <URL> <email>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response_body = send_post_request(url, email)
+
+    print("Response body:")
+    print(response_body)
+
 
--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,7 +1,20 @@
 import requests
 import sys
-url = sys.argv[1]
-email = sys.argv[2]
-data = {'email': email}
-response = requests.post(url, data=data)
-print(response.text)
+
+def send_post_request(url, email):
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    return response.text
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: hr@holbertonschool.com <URL> <email>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    response_body = send_post_request(url, email)
+
+    print("Response body:")
+    print(response_body)
