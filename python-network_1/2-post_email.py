43087f79319f91525826dc2af566c7b128e26b4d
TshegoR24 <cavinrakoma@gmail.com>
2023-08-10 18:17:23 +0200

    python networks

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import requests
 import sys
 
@@ -7,14 +9,15 @@ def send_post_request(url, email):
     return response.text
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: hr@holbertonschool.com <URL> <email>")
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <URL>")
         sys.exit(1)
     
     url = sys.argv[1]
-    email = sys.argv[2]
+    email = "hr@holbertonschool.com"
 
     response_body = send_post_request(url, email)
 
     print("Response body:")
     print(response_body)
+
