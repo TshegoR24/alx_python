645df1e51465027b4b2dcc648717e254e1da07f2
TshegoRakoma <cavinrakoma@gmail.com>
2023-09-26 11:51:49 +0200

    python web framework

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,47 +1,15 @@
 #!/usr/bin/python3
 
-"""
-This script fetches the status information from a URL using the 'requests' package
-and displays the body of the response with tabulation.
-"""
-
 import requests
 
-class StatusFetcher:
-    """
-    A class to fetch and display status information from a URL.
-    """
+response = requests.get('https://alu-intranet.hbtn.io/status')
 
-    def __init__(self, url):
-        """
-        Initialize the StatusFetcher with the given URL.
-
-        Args:
-            url (str): The URL to fetch the status from.
-        """
-        self.url = url
-
-    def fetch_and_display_status(self):
-        """
-        Fetches the status information from the URL and displays the response body.
-
-        Returns:
-            None
-        """
-        response = requests.get(self.url)
-
-        if response.status_code == 200:
-            data = response.json()
-            print("Body response:")
-            print("\t- type:", type(data))
-            print("\t- content:", data)
-        else:
-            print("Error:", response.status_code)
-
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    status_fetcher = StatusFetcher(url)
-    status_fetcher.fetch_and_display_status()
+if response.status_code == 200:
+    data = response.json()
+    for key, value in data.items():
+        print(f'- {key}: {value}')
+else:
+    print(f'Error: {response.status_code}')
 
 
 
