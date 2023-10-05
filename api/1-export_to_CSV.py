f4a0b2a1bf368d4620d89bd9ddc4a1a290cbed90
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-05 13:03:50 +0200

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,24 +3,35 @@
 Exporting the data from the api requests to the csv file
 """
 
-import csv
+python
 import requests
+import csv
 import sys
-# Get the employee ID from the command line
-employee_id = int(sys.argv[1])
-# Get the employee's TODO list
-response = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
-todos = response.json()
-# Get the employee's name
-response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-employee = response.json()
-employee_name = employee["name"]
-# Create a CSV file
-with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
-    # Create a CSV writer
-    csvwriter = csv.writer(csvfile)
-    # Write the header row
-    csvwriter.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-    # Write the data rows
-    for todo in todos:
-        csvwriter.writerow([employee_id, employee_name, todo["completed"], todo["title"]])
+
+def get_employee_details(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    return response.json()
+
+def get_employee_todos(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    return response.json()
+
+def export_to_csv(employee_id, employee_details, employee_todos):
+    file_name = f"{employee_id}.csv"
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for todo in employee_todos:
+            writer.writerow([employee_id, employee_details["username"], todo["completed"], todo["title"]])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py EMPLOYEE_ID")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    employee_details = get_employee_details(employee_id)
+    employee_todos = get_employee_todos(employee_id)
+    export_to_csv(employee_id, employee_details, employee_todos)
