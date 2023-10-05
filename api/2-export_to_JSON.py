63f0f43bd0fc6f14e1d35809cfc795a3b2f66ef2
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-05 13:07:15 +0200

    API

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 
-import json
+python
 import requests
-import sys
-# Get the employee ID from the command line
-employee_id = sys.argv[1]
-# Get the employee's TODO list
-response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id))
-todos = response.json()
-# Get the employee's name
-response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-employee = response.json()
-# Create a dictionary of the employee's TODO list
-employee_todos = {}
-employee_todos[employee["id"]] = []
-# Add each TODO to the dictionary
-for todo in todos:
-    employee_todos[employee["id"]].append({"task": todo["title"], "completed": todo["completed"], "username": employee["username"]})
-# Write the dictionary to a JSON file
-with open("{}.json".format(employee_id), "w") as jsonfile:
-    json.dump(employee_todos, jsonfile)
+import json
+
+def get_employee_tasks(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    return response.json()
+
+def export_to_json(employee_id, tasks):
+    file_name = f"{employee_id}.json"
+    with open(file_name, 'w') as file:
+        json.dump(tasks, file)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py EMPLOYEE_ID")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    tasks = get_employee_tasks(employee_id)
+    export_to_json(employee_id, tasks)
