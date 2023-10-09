4a3d343fb061b3db1985f9bb4f1b1bdda8e2874b
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-09 19:02:11 +0200

    python network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,15 +3,20 @@
 import requests
 
 url = 'https://alu-intranet.hbtn.io/status'
-response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    print("Body response:")
-    print("\t- type:", type(data))
-    print("\t- content:", data)
-else:
-    print(f"Error: Unable to fetch data from {url}. Status code: {response.status_code}")
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+
+    # Display the response body with tabulation before each line
+    lines = response.text.split('\n')
+    formatted_response = '\n'.join(['\t' + line for line in lines])
+
+    print(formatted_response)
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+
    
 
 
