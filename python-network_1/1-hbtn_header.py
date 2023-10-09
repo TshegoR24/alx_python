f1c3a5e8adef54c5214da8c36b3e53a44f4c80b4
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-09 19:28:13 +0200

    python network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 
-
+python
 import requests
 import sys
 
-def main():
-    url = sys.argv[1]
-    response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-    if request_id:
-        print(f'X-Request-Id: {request_id}')
-    else:
-        print('X-Request-Id not found in response header')
+def get_request_id(url):
+    try:
+        response = requests.get(url)
+        request_id = response.headers.get('X-Request-Id')
+        if request_id:
+            print(f'The value of X-Request-Id is: {request_id}')
+        else:
+            print('The X-Request-Id header is not present in the response.')
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred: {e}')
+        sys.exit(1)
 
 if __name__ == '__main__':
-    main()
+    url = 'https://www.example.com'
+    get_request_id(url)
 
 
 
