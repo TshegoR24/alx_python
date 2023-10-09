1821f11e22e83cf97ccb30af3cb8dc3150f634b1
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-09 19:32:46 +0200

    pyhon network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-
-python
 import requests
 import sys
 
-def get_request_id(url):
+def get_x_request_id(url):
     try:
+        # Send a GET request to the URL
         response = requests.get(url)
-        request_id = response.headers.get('X-Request-Id')
-        if request_id:
-            print(f'The value of X-Request-Id is: {request_id}')
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Check if the 'X-Request-Id' header is present in the response
+            if 'X-Request-Id' in response.headers:
+                # Print the value of the 'X-Request-Id' header
+                print(f"X-Request-Id: {response.headers['X-Request-Id']}")
+            else:
+                print("X-Request-Id header not found in the response.")
         else:
-            print('The X-Request-Id header is not present in the response.')
+            print(f"Request failed with status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
-        print(f'An error occurred: {e}')
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
         sys.exit(1)
 
-if __name__ == '__main__':
-    url = 'https://www.example.com'
-    get_request_id(url)
+    url = sys.argv[1]
+    get_x_request_id(url)
+
+
 
 
 
