465566e21575d33ff306cc8f60d2e414fa2512a7
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-09 21:14:16 +0200

    python network

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,26 +1,41 @@
+#!/usr/bin/python3
+
 import sys
 import requests
 
-def search_user_by_letter(letter):
-    url = "http://0.0.0.0:5000/search_user"
-    params = {'q': letter}
+def get_user_id(username, password):
+    # Define the GitHub API URL for the authenticated user
+    api_url = f'https://api.github.com/user'
 
-    response = requests.post(url, params=params)
+    # Create a basic authentication header with the username and personal access token
+    auth = (username, password)
 
-    try:
-        response_json = response.json()
-        if response_json:
-            user_id = response_json.get('id', '')
-            user_name = response_json.get('name', '')
-            if user_id and user_name:
-                print(f"[{user_id}] {user_name}")
-            else:
-                print("No result")
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+    # Send a GET request to the GitHub API using Basic Authentication
+    response = requests.get(api_url, auth=auth)
 
-if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search_user_by_letter(letter)
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response and extract the user ID
+        user_data = response.json()
+        user_id = user_data['id']
+        return user_id
+    else:
+        print(f"Failed to retrieve user data. Status code: {response.status_code}")
+        return None
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python github_user_id.py <username> <personal_access_token>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    personal_access_token = sys.argv[2]
+
+    user_id = get_user_id(username, personal_access_token)
+
+    if user_id is not None:
+        print(f"User ID for {username}: {user_id}")
+    else:
+        print("User ID retrieval failed.")
+
+
