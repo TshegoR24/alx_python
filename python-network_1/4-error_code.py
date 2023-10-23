738010f47c5b7e14aa731ae39cdf99166cbbcbb8
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-23 10:21:42 +0200

    python network

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-
-python
-import requests
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 import sys
+import requests
 
-def search_user(letter):
-    url = "http://0.0.0.0:5000/search_user"
-    params = {"q": letter}
-    response = requests.post(url, data=params)
-
-    if response.status_code == 200:
-        try:
-            json_data = response.json()
-            if json_data:
-                for user in json_data:
-                    print(f"[{user['id']}] {user['name']}")
-            else:
-                print("No result")
-        except ValueError:
-            print("Not a valid JSON")
-    else:
-        print(f"Request failed with status code {response.status_code}")
 
 if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search_user(letter)
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
