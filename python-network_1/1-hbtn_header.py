fc06a441a0439bbe8d2cbf9236ebf19a406800dd
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-23 10:10:10 +0200

    python network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-import requests
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
+import urllib.request
 
-def get_x_request_id(url):
-    try:
-        # Send a GET request to the URL
-        response = requests.get(url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Check if the 'X-Request-Id' header is present in the response
-            if 'X-Request-Id' in response.headers:
-                # Print the value of the 'X-Request-Id' header
-                print(f"X-Request-Id: {response.headers['X-Request-Id']}")
-            else:
-                print("X-Request-Id header not found in the response.")
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    get_x_request_id(url)
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
 
 
 
