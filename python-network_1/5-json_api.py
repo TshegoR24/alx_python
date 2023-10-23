0a0c7cbc728c47506885c4491d94a88cb049affb
TshegoRakoma <cavinrakoma@gmail.com>
2023-10-23 10:25:41 +0200

    python network

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
-
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
 import requests
 
-def get_user_id(username, password):
-    # Define the GitHub API URL for the authenticated user
-    api_url = f'https://api.github.com/user'
 
-    # Create a basic authentication header with the username and personal access token
-    auth = (username, password)
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    # Send a GET request to the GitHub API using Basic Authentication
-    response = requests.get(api_url, auth=auth)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response and extract the user ID
-        user_data = response.json()
-        user_id = user_data['id']
-        return user_id
-    else:
-        print(f"Failed to retrieve user data. Status code: {response.status_code}")
-        return None
-
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python github_user_id.py <username> <personal_access_token>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    personal_access_token = sys.argv[2]
-
-    user_id = get_user_id(username, personal_access_token)
-
-    if user_id is not None:
-        print(f"User ID for {username}: {user_id}")
-    else:
-        print("User ID retrieval failed.")
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
 
 
